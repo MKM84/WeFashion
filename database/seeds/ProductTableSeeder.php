0000c8a8452456c1b631d3265d1125664897ae41
Majed <khreimmajed@gmail.com>
2021-05-25 21:19:19 +0200

    Add tow values to the categories table then associate those values to the product

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -11,7 +11,31 @@ class ProductTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Product::class, 80)->create();
+
+
+
+        // Storage::disk('local')->delete(Storage::allFiles());
+
+        App\Category::create([
+            'category' => 'homme'
+        ]);
+        App\Category::create([
+            'category' => 'femme'
+        ]);
+
+        // create 80 products from factory
+        factory(App\Product::class, 30)->create()->each(function ($product) {
+
+            // Associate a category to a product
+            $category = App\Category::find(rand(1, 2));
+
+            // for every prooduct one category
+            $product->category()->associate($category);
+
+            $product->save(); // save the association in the DB 
+
+           
+        });
 
     }
 }
