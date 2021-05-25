2d5a4e9c60f1e862ca45db44792a965b2ecbd9e0
Majed <khreimmajed@gmail.com>
2021-05-25 16:42:23 +0200

    Create & migrate the products table

---
+++ database/migrations/2021_05_25_135544_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->text('description')->nullable(); // optional
+            $table->decimal('price', 4, 2);
+            $table->enum('size', ['XS', 'S', 'M', 'L', 'XL'])->default('M');
+            $table->enum('visibility', ['published', 'unpublished'])->default('unpublished');
+            $table->enum('status', ['sold', 'standard'])->default('standard');
+            $table->string('reference', 16)->unique(); // uniqe reference
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
