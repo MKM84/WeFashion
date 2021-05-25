145c475ed2570ef73be4a0fa14734c4306ee9ce8
Majed <khreimmajed@gmail.com>
2021-05-25 18:11:52 +0200

    Add fillable to Product model

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+    protected $fillable = [
+        'name', 'description', 'price', 'size', 'visibility', 'sttatus', 'reference', 'category_id'
+    ];
+
     public function category()
     {
         return $this->hasOne(Category::class);
