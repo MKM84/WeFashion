e5289d63162647ae03b3f8ce504166221a78f6e0
Majed <khreimmajed@gmail.com>
2021-05-26 13:09:24 +0200

    Install npm, broswserSync and create style.scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,14 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sass('resources/assets/sass/style.scss', 'public/css')
+       .browserSync({
+        proxy: 'localhost:8000',
+        files: [
+            'app/**/*.php',
+            'resources/views/**/*.php',
+            'public/assets/js/**/*.js',
+            'public/assets/css/**/*.css'
+        ]
+    });
